5645550416665bba786c8f327d96e4b861d1867c
Skrepa <kosmodan01@gmail.com>
2016-11-28 13:04:33 +0400

    Complited

---
+++ src/taskManager/View/View.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import Model.Journal;
+import Model.Task;
+
+/**
+ *
+ * @author Даниил
+ */
+public interface View 
+{
+    void update(Journal journal);
+    
+    void showException(Exception e);
+    
+    void setVisible(boolean b);
+    
+    public void showNotification(Task t) ;
+}
